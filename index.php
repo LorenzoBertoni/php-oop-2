5c866f8c872d907d0c7d3ec7cdef179c62b21753
LorenzoBertoni <lorenzo.berto99@hotmail.it>
2022-09-23 15:22:08 +0200

    add: comments

--- index.php
+++ index.php
@@ -1,39 +1,43 @@
 <?php
     require_once __DIR__ . '/Classes/Customer.php';
     require_once __DIR__ . '/Classes/Employee.php';
-
+    
+    //***CLIENTE***
     $customer = new Customer("Mario Rossi", "mariorossi@gmail.com");
+
+    //*eccezione sollevata in caso di password poco sicura
     try {
         $customer->setPassword("roots");
     } catch (Exception $e) {
         echo 'Error: ' . $e->getMessage();
     }
-    
+
+    //*eccezione sollevata in caso di accesso negato
     try {
         $customer->setLevel("customer");
     } catch (Exception $e) {
         echo $customer->nickName . "=>" . "Livello di accesso: Cliente => " . $e->getMessage() . "<br>";
     }
-    //var_dump($customer);
 
-
-
+    //***DIPENDENTE***
     $employee = new Employee("Luca Bianchi", "lucabianchi@gmail.com");
 
+    //*eccezione sollevata in caso di password poco sicura
     try {
         $employee->setPassword("password");
     } catch (Exception $e) {
         echo 'Error: ' . $e->getMessage();
     }
 
+    //*eccezione sollevata in caso di accesso negato
     try {
         $employee->setLevel("employee");
         echo $employee->nickName . "=>" . "Livello di accesso: Dipendente => Puoi accedere all'area riservata ai dipendenti <br>";
     } catch (Exception $e) {
         echo "Livello di accesso: " . $e->getMessage() . "<br>";
     }
-    //var_dump($employee);
 
+    //*stampa delle informazioni
     echo "<br><br>";
     $customer->getUserInfo();
     echo "<br><br>";
